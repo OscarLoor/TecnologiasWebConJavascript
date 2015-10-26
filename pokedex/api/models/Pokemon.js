8a1fe6278096ab08af8cff7f76a08b805dadd310
Oscar Loor <dwquito@hotmail.com>
2015-10-26 14:05:57 -0500

    Cree la tabla para la pokedex
    Modificación del modelo pokemon

--- pokedex/api/models/Pokemon.js
+++ pokedex/api/models/Pokemon.js
@@ -1,14 +1,36 @@
 /**
-* Pokemon.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Pokemon.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
-  attributes: {
+    attributes: {
+        nombre: {
+            type: 'string',
+            unique: true,
+            required: true
+        },
 
-  }
+        tipo: {
+            type: 'string',
+            enum: ['agua', 'fuego', 'psiquico', 'hierba', 'electrico']
+
+        },
+
+        habilidadEspecial: {
+            type: 'string',
+            defaultsTo: 'Ninguno'
+        },
+
+        numeroPokemon: {
+            type: 'integer',
+            required: true
+        },
+        fechaVista: {
+            type: 'date'
+        }
+    }
 };
-
